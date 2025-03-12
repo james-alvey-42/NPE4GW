c0a4389a2598685d9d3d6b5347956e5332768abb
Krish Nanavati <knanavati126@gmail.com>
2025-03-12 17:03:32 +0000

    plot posteriors from the logratios

--- Tools/Other/get_logratios.py
+++ Tools/Other/get_logratios.py
@@ -1,14 +1,15 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 lrs = pd.read_pickle('/Users/knana/Documents/GitHub/NPE4GW/Tools/Other/logratios_R1 (1)')
 print(lrs.logratios.shape)
 
 #plot histogram of logratios
-plt.figure(figsize=(10, 5))
-plt.title("TMNRE logratios")
-plt.hist(lrs.logratios[:,2], bins=30, density=True, alpha=0.7, label="logratios")
-plt.xlabel("Chirp mass")
-plt.ylabel("Density")
-plt.legend()
+plt.figure(figsize=(15, 8))
+for idx in range(len(lrs.parnames)):
+    ax = plt.subplot(5, 3, idx + 1)
+    logratios = lrs.logratios[:, idx]
+    params = lrs.params[:, idx, 0]
+    plt.hist(params, weights=np.exp(logratios.numpy()), bins=100)
 plt.show()
