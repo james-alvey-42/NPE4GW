2a862e53860281d4a60581adcd0b53b0d4b48060
Krish Nanavati <knanavati126@gmail.com>
2025-03-12 16:45:55 +0000

    plot the logratios

--- Tools/Other/get_logratios.py
+++ Tools/Other/get_logratios.py
@@ -1,3 +1,14 @@
 import pandas as pd
-lrs = pd.read_pickle('logratios_R1')
-print(type(lrs))
+import matplotlib.pyplot as plt
+
+lrs = pd.read_pickle('/Users/knana/Documents/GitHub/NPE4GW/Tools/Other/logratios_R1 (1)')
+print(lrs.logratios.shape)
+
+#plot histogram of logratios
+plt.figure(figsize=(10, 5))
+plt.title("TMNRE logratios")
+plt.hist(lrs.logratios[:,2], bins=30, density=True, alpha=0.7, label="logratios")
+plt.xlabel("Chirp mass")
+plt.ylabel("Density")
+plt.legend()
+plt.show()
